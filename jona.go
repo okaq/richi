e87d0a62382c5a0305a537ed841ba1f75df9c9d1
AQ <aq@okaq.com>
2017-03-30 13:13:12 -0400

    Server json unique user id decode

--- jona.go
+++ jona.go
@@ -4,6 +4,7 @@
 package main
 
 import (
+    "encoding/json"
     "fmt"
     "net/http"
 )
@@ -12,6 +13,10 @@ var (
     INDEX = "kona.html"
 )
 
+type Uniq struct {
+    Date string
+    Id string
+}
 
 func HiwaHandler(w http.ResponseWriter, r *http.Request) {
     fmt.Println(r)
@@ -24,10 +29,21 @@ func ScooHandler(w http.ResponseWriter, r *http.Request) {
     w.Write([]byte("scoobah party!"))
 }
 
+func IdHandler(w http.ResponseWriter, r *http.Request) {
+    fmt.Println(r)
+    var u Uniq
+    err := json.NewDecoder(r.Body).Decode(&u)
+    if err != nil {
+        fmt.Println(err)
+    }
+    fmt.Println(u)
+}
+
 func main() {
     fmt.Println("starting server on localhost:8080")
     http.HandleFunc("/", HiwaHandler)
     http.HandleFunc("/a", ScooHandler)
+    http.HandleFunc("/b", IdHandler)
     http.ListenAndServe(":8080", nil)
 }
 
