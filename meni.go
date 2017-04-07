94bb5140a3db9ddd78040f74114274cff791b3af
AQ <aq@okaq.com>
2017-04-07 09:20:22 -0400

    Players list atomic

--- meni.go
+++ meni.go
@@ -5,11 +5,31 @@ package main
 import (
     "fmt"
     "net/http"
+    "sync"
 )
 
 const (
     INDEX = "lumi.html"
 )
+
+var (
+    M *Marble
+)
+
+type Marble struct {
+    Count int
+    Max int
+    List []string
+    sync.RWMutex
+}
+
+func NewMarble() *Marble {
+    m := Marble{}
+    m.Count = 0
+    m.Max = 64
+    m.List = make([]string, m.Max)
+    return &m
+}
 
 func MeniHandler(w http.ResponseWriter, r *http.Request) {
     fmt.Println(r)
@@ -18,6 +38,8 @@ func MeniHandler(w http.ResponseWriter, r *http.Request) {
 
 func main() {
     fmt.Println("serving on localhost:8080")
+    M = NewMarble()
+    fmt.Println(M)
     http.HandleFunc("/", MeniHandler)
     http.ListenAndServe(":8080", nil)
 }
