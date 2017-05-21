346c5c6aa3f2fa0dabc0aa0d55cb63817846c171
AQ <aq@okaq.com>
2017-05-21 09:10:08 -0400

    New file tola.html

---
+++ ubia.go
@@ -0,0 +1,26 @@
+// okaq web server
+// 2017-05-22
+package main
+
+import (
+    "fmt"
+    "net/http"
+    "time"
+)
+
+const (
+    INDEX = "tola.html"
+)
+
+func UbiaHandler(w http.ResponseWriter, r *http.Request) {
+    fmt.Println(r)
+    http.ServeFile(w, r, INDEX)
+}
+
+func main() {
+    fmt.Println("web server started on localhost:8080")
+    t0 := time.Now()
+    fmt.Printf("begin time: %s\n", t0.String())
+    http.HandleFunc("/", UbiaHandler)
+    http.ListenAndServe(":8080", nil)
+}
