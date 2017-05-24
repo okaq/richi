03c20d54605274b9e097633a7d426a864c4df9a7
AQ <aq@okaq.com>
2017-05-24 12:26:24 -0400

    Player cache design

--- ubia.go
+++ ubia.go
@@ -3,6 +3,7 @@
 package main
 
 import (
+    "bufio"
     "fmt"
     "net/http"
     "time"
@@ -12,6 +13,20 @@ const (
     INDEX = "tola.html"
 )
 
+type Player struct {
+    Id string
+    Pixels []int
+}
+
+func NewPlayer() *Player {
+    return &Player{}
+}
+
+type Cache struct {
+    Players map[string]*Player
+    sync.Mutex
+}
+
 func UbiaHandler(w http.ResponseWriter, r *http.Request) {
     fmt.Println(r)
     http.ServeFile(w, r, INDEX)
@@ -19,6 +34,12 @@ func UbiaHandler(w http.ResponseWriter, r *http.Request) {
 
 func PidHandler(w http.ResponseWriter, r *http.Request) {
     fmt.Println(r)
+    s0 := bufio.NewScanner(r.Body)
+    s0.Scan()
+    t0 := s0.Text()
+    b0 := s0.Bytes()
+    fmt.Printf("Length (in bytes) of the request body is: %d.\n", len(b0))
+    fmt.Printf("Request body data: %s.\n", t0)
     w.Header().Set("Content-type","text/plain")
     w.Write([]byte("oko"))
 }
