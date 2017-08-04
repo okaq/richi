8f1a68618f032ef43a9309dd7a402e27f78474c7
AQ <aq@okaq.com>
2017-08-04 08:16:44 -0400

    Ain web server go

--- ninki/ain.go
+++ ninki/ain.go
@@ -6,8 +6,21 @@ package main
 
 import (
     "fmt"
+    "net/http"
+    "time"
 )
 
+const (
+    ZIN = "zin.html"
+)
+
+func ZinHandler(w http.ResponseWriter, r *http.Request) {
+    fmt.Println(r)
+    http.ServeFile(w,r,ZIN)
+}
+
 func main() {
-    fmt.Println("hello ninki")
+    fmt.Printf("localhost:8080 zin web start: %s\n", time.Now().String())
+    http.HandleFunc("/", ZinHandler)
+    http.ListenAndServe(":8080", nil)
 }
