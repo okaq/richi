83b6eb52d2033577b1c73cb99259698a07c22056
AQ <aq@okaq.com>
2017-10-25 08:46:46 -0400

    Debug mode true and time stamp

--- ninki/eye/aye.go
+++ ninki/eye/aye.go
@@ -5,8 +5,16 @@ package main
 
 import (
     "fmt"
+//     "net/http"
+    "time"
+)
+
+const (
+    DEBUG = true
+    EYE = "zye.html"
 )
 
 func main() {
     fmt.Println("okaq web eye live...")
+    fmt.Printf("localhost:8080 start time: %s\n", time.Now().String())
 }
