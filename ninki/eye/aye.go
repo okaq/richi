cf9a45d107c6f769b05e249446e5e7abb58373c0
AQ <aq@okaq.com>
2017-10-28 13:35:14 -0400

    Player id server handler

--- ninki/eye/aye.go
+++ ninki/eye/aye.go
@@ -21,11 +21,21 @@ func EyeHandler(w http.ResponseWriter, r *http.Request) {
     http.ServeFile(w,r,EYE)
 }
 
+func PidHandler(w http.ResponseWriter, r *http.Request) {
+    // ready player id
+    if DEBUG {
+        fmt.Println(r)
+    }
+    w.Header().Set("Content-type", "text/plain")
+    w.Write([]byte("ok pid one"))
+}
+
 func main() {
     if DEBUG {
         fmt.Println("okaq web eye live...")
         fmt.Printf("localhost:8080 start time: %s\n", time.Now().String())
     }
     http.HandleFunc("/", EyeHandler)
+    http.HandleFunc("/a", PidHandler)
     http.ListenAndServe(":8080", nil)
 }
