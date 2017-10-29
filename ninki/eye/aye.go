83cb4793a396e84d197b79d7ca2836077e84b668
AQ <aq@okaq.com>
2017-10-29 15:41:10 -0400

    Server data handler

--- ninki/eye/aye.go
+++ ninki/eye/aye.go
@@ -30,6 +30,18 @@ func PidHandler(w http.ResponseWriter, r *http.Request) {
     w.Write([]byte("ok pid one"))
 }
 
+func DataHandler(w http.ResponseWriter, r *http.Request) {
+    // cache data sent
+    // update global state
+    // write details to client
+    if DEBUG {
+        fmt.Println(r)
+    }
+    j0 := nil
+    w.Header().Set("Content-type", "application/json")
+    w.Write(j0)
+}
+
 func main() {
     if DEBUG {
         fmt.Println("okaq web eye live...")
@@ -37,5 +49,6 @@ func main() {
     }
     http.HandleFunc("/", EyeHandler)
     http.HandleFunc("/a", PidHandler)
+    http.HandleFunc("/b", DataHandler)
     http.ListenAndServe(":8080", nil)
 }
