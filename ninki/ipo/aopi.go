693a6c5f6215c5b1ce99239e034009105aabab6d
AQ <aq@okaq.com>
2017-11-04 16:00:42 -0400

    Web handler

--- ninki/ipo/aopi.go
+++ ninki/ipo/aopi.go
@@ -1,7 +1,7 @@
 // okaq web server
 // IEX IPO SIM
 // https://iextrading.com/
-// AQ <aqokaq.com>
+// AQ <aq@okaq.com>
 // 2017-11-04
 
 package main
@@ -11,6 +11,17 @@ import (
     "time"
 )
 
+const (
+    INDEX = "zopi.html"
+)
+
+func AopiHandler(w http.ResponseWriter, r *http.Request) {
+    fmt.Println(r)
+    http.ServeFile(w,r,INDEX)
+}
+
 func main() {
     fmt.Printf("stock application started at: %s\n", time.Now().String())
+    http.HandleFunc("/", AopiHandler)
+    http.ListenAndServe(":8080", nil)
 }
