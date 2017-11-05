d2c3ba0e52a383d1813fb319f786477d53807005
AQ <aq@okaq.com>
2017-11-05 08:54:13 -0500

    New file zevi.html

---
+++ ninki/csp/aevi.go
@@ -0,0 +1,15 @@
+// okaq web server
+// csp nano game
+// AQ <aq@okaq.com>
+// 2017-11-06
+
+package main
+
+import (
+    "fmt"
+    "time"
+)
+
+func main() {
+    fmt.Printf("csp web serve start %s\n", time.Now().String())
+}
