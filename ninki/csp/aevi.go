3fce544bd920ba142350b1e573fca3d249c5d815
AQ <aq@okaq.com>
2017-11-05 17:45:28 -0500

    Web handler root

--- ninki/csp/aevi.go
+++ ninki/csp/aevi.go
@@ -8,8 +8,20 @@ package main
 import (
     "fmt"
     "time"
+    "net/http"
 )
+
+const (
+    INDEX = "zevi.html"
+)
+
+func AeviHandler(w http.ResponseWriter, r *http.Request) {
+    fmt.Println(r)
+    http.ServeFile(w,r,INDEX)
+}
 
 func main() {
     fmt.Printf("csp web serve start %s\n", time.Now().String())
+    http.HandleFunc("/", AeviHandler)
+    http.ListenAndServe(":8080", nil)
 }
