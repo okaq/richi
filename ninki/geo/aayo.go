26f4b6003fdbeba14f1c79775d4668d746fd3cd3
AQ <aq@okaq.com>
2017-12-20 10:01:08 -0500

    New file zayo.html

---
+++ ninki/geo/aayo.go
@@ -0,0 +1,27 @@
+// okaq web server
+// geo math
+// AQ <aq@okaq.com>
+// 2017-12-20
+
+package main
+
+import (
+    "fmt"
+    "net/http"
+    "time"
+)
+
+const (
+    INDEX = "zayo.html"
+)
+
+func AayoHandler(w http.ResponseWriter, r *http.Request) {
+    fmt.Println(r)
+    http.ServeFile(w,r,INDEX)
+}
+
+func main() {
+    fmt.Printf("okaq geo web start\n%s\n", time.Now().String())
+    http.HandleFunc("/", AayoHandler)
+    http.ListenAndServe(":8080", nil)
+}
